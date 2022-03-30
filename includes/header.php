719e61a662408ccdd8b4357273ac6ec804e77b20
stephane71183101 <stephane71183101@gmail.com>
2022-03-30 13:51:22 +0200

    css for index.php and question-01.php

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,19 @@
 <body>
-    
+
 <header>
     <nav class="navbar sticky-top navbar-light style="background-color: #ffffff;>
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="/images/logo_schoolquest.png" alt="" width="288" height="92">
-            </a>
+            <div class="container">
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-10">
+                        <a class="navbar-brand" href="#">
+                            <img src="/images/logo_schoolquest.png" alt="" width="288" height="92">
+                        </a>
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+            </div>
         </div>
     </nav>
 </header>
---
+++ question-01.php
@@ -0,0 +1,73 @@
+<!--author: Stéphane-->
+<?php include "includes/head.php"; ?>
+
+<?php include "includes/header.php";?>
+<main>
+    <!-- Hier kommt noch der Data-Collector-->
+    <div>
+        <div class="container-fluid bg-warning text-dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-10">
+                        <h1 id="questionTitle">Frage 1</h1>
+                        <p id="questionWording">Du bist neu in der Schweiz und hast Papierkram zu erledigen. Wonach erkundigt sich das Amt, wenn es fragt: "Werden Sie betrieben?"<p>
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <form action="question-09.php" method="post" onsubmit="return validateQuestion('single-choice-0', 'single-choice');">       
+                <div class="row">
+                    <div class="col-1"></div>
+                        <div class="col-10" id="answerPanel">
+                            <div class="form-check">
+                                <input type="radio" class="form-check-input" id="single-choice-1" name="single-choice" value="0">
+                                <label class="form-check-label" for="single-choice-1"><p>Haben Sie Schulden?</p></label>
+                            </div>
+
+                            <div class="form-check">
+                                <input type="radio" class="form-check-input" id="single-choice-2"name="single-choice"value="1">
+                                <label class="form-check-label" for="single-choice-2"><p>Sind Sie selbstständig?</p></label>
+                            </div>
+
+                            <div class="form-check">
+                                <input type="radio" class="form-check-input" id="single-choice-3" name="single-choice" value="0">
+                                <label class="form-check-label" for="single-choice-3"><p>Haben Sie eine Festanstellung?</p></label>
+                            </div>
+
+                            <div class="form-check">
+                                <input type="radio" class="form-check-input" id="single-choice-4" name="single-choice" value="0">
+                                <label class="form-check-label" for="single-choice-4"><p>Sind sie haftpflichtversichert?</p></label>
+                            </div>
+                        </div> 
+                        <div class="col-1"></div>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-1"></div>
+
+                            <div class="col-10">
+                                <div>
+                                    <input type="hidden" name="lastPageID" value="question-9">
+                                    <p id="validation-warning" class="warning"></p>              
+                                </div>
+
+                                <div>
+                                    <input type="hidden" name="lastPageID" value="question-13">
+                                    <input type="hidden" id="achivedPoints" name="achivePoints">
+                                    <p id="validation-warning" class="warning"></p>
+                                    <button type="submit" id=startButton class="btn btn-outline-danger buttons">Weiter</button>
+                                </div>
+                            </div>
+                            </div><div class="col-1">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+<?php include "includes/footer.php"; ?>
