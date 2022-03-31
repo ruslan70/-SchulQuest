53af8887f52b838860561a596464312dfacb9f4e
stephane71183101 <stephane71183101@gmail.com>
2022-03-31 13:36:15 +0200

    conflict resolution

--- question-03.php
+++ question-03.php
@@ -60,11 +60,7 @@
                             <div class="col-10">
                                 <div>
                                     <input type="hidden" name="lastPageID" value="question-03">
-<<<<<<< HEAD
                                     <input type="hidden" id="achievedPoints" name="achievedPoints">
-=======
-                                    <input type="hidden" id="achivedPoints" name="achivePoints">
->>>>>>> origin/master
                                     <p id="validation-warning" class="warning"></p>
                                     <button type="submit" class="btn btn-outline-danger buttons">Weiter</button>
                                 </div>
