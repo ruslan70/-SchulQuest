823b2a634768eb4a572a3c08da1399e01bcf1437
ruslan70 <ruslan.khatuev70@gmail.com>
2022-04-02 21:13:47 +0200

    Annpasung klein

--- includes/evaluate.php
+++ includes/evaluate.php
@@ -7,11 +7,11 @@ function printResult() {
     $totalPoints = evaluateQuestions();
     
    if ($totalPoints < 9) {
-        echo "<h3>" . "Du muss noch viel nachholen..." . "</h3>";
+        echo "<h3>" . "Du musst noch viel nachholen..." . "</h3>";
 
     }
     elseif ($totalPoints < 13) {
-        echo "<h3>" . "Gut, Du kennst schon viel!" . "</h3>";
+        echo "<h3>" . "Gut, Du weisst schon viel!" . "</h3>";
  
     }
     elseif ($totalPoints > 13) {
